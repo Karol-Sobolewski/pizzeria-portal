3a5552ceabc5a488ab7755822becfe45e47fc41d
Karol <karolsobolewski92@gmail.com>
2020-07-23 18:57:09 +0200

    Add pages routing

--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 //import styles from './HappyHourAd.scss';
-import {NavLink, Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 const PageNav = ({children}) => (
   <nav>
