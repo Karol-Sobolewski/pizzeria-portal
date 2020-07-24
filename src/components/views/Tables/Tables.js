dcd9fba1c30d4d295d4b874d886156ac17402234
Karol <karolsobolewski92@gmail.com>
2020-07-24 19:59:03 +0200

    Fix (?) pages routing

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import styles from './Tables.module.scss';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
-const Tables = (/*{title}*/) => (
+const Tables = (id) => (
   <div className={styles.component}>
     <h2>Tables view</h2>
+    <Link to={`/booking/${id}`} className={styles.link}>Booking ID:</Link>
+    <Link to={`/booking/new`} className={styles.link}>New booking</Link>
+    <Link to={`/events/${id}`} className={styles.link}>Event ID:</Link>
+    <Link to={`tables/events/new`} className={styles.link}>New event</Link>
   </div>
 );
 
-/*Login.propTypes = {
-  title: PropTypes.string,
+Tables.propTypes = {
+  id: PropTypes.string,
 };
-*/
+
 export default Tables;
--- src/components/views/Waiter/Waiter.js
+++ src/components/views/Waiter/Waiter.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import styles from './Waiter.module.scss';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
-const Waiter = (/*{title}*/) => (
+const Waiter = (id) => (
   <div className={styles.component}>
     <h2>Waiter view</h2>
+    <Link to={`waiter/order/${id}`} className={styles.link}>Order ID:</Link>
+    <Link to={`waiter/order/new`} className={styles.link}>New order</Link>
   </div>
 );
 
-/*Login.propTypes = {
-  title: PropTypes.string,
+Waiter.propTypes = {
+  id: PropTypes.string,
 };
-*/
+
 export default Waiter;
